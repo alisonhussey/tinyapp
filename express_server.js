274dc3bd4da684f39949d1e90005f8defdf3b61e
alisonhussey <alisonhussey@gmail.com>
2020-12-08 01:11:43 +0000

    Added POST Route to Receive the Form Submission and a function to return a random alphanumeric number

--- express_server.js
+++ express_server.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+return Math.random().toString(36).substring(2, 8)
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -32,6 +36,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 // app.get("/hello", (req, res) => {
